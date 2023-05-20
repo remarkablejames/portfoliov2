859b5a0fae82acbb4be34275b9441ca444d6cfb3
James Niyongira <lascasasjames85@gmail.com>
2023-05-19 22:16:49 -0400

    fixed production build issues

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 // Import all css files
-import './assets/css/bootstrap.min.css';
+ import './assets/css/bootstrap.min.css';
 import './assets/css/aos.css';
 import './assets/css/style.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
